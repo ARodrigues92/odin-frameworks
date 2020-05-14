2c7227802c219fcf307c97b5083bcc0baf936bf2
André Rodrigues <arodrigues92@protonmail.ch>
2020-05-14 19:05:52 +0100

    Implement book adding

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import sampleData from './modules/sample-data';
-import './styles/index.css';
 import './styles/reset.css';
+import './styles/index.css';
 
 function EditBook(props) {
   return <button type="button">Edit</button>;
@@ -32,10 +32,10 @@ function ReadCheckBox(props) {
   return (
     <div className="checkbox-container book-elements">
       <form>
-        <label htmlFor="read">
-          Is Read?
+        <label htmlFor="read-status">
+          Is Read
           <input
-            id="read"
+            id="read-status"
             name="read"
             type="checkbox"
             checked={isRead}
@@ -78,14 +78,88 @@ function Book(props) {
   );
 }
 
+function NewBook(props) {
+  const { onClick } = props;
+  return (
+    <div className="book new-book">
+      <button
+        className="add-book-button"
+        type="button"
+        onClick={() => onClick()}
+      >
+        +
+      </button>
+    </div>
+  );
+}
+
+function Form(props) {
+  const { showForm, handleSubmit } = props;
+  const modalClasses = showForm ? 'modal' : 'modal display-none';
+  return (
+    <div className={modalClasses}>
+      <div className="modal-main">
+        <form className="form" onSubmit={handleSubmit}>
+          <label className="label" htmlFor="title">
+            Title
+            <input
+              className="input"
+              id="title"
+              name="title"
+              type="text"
+              required
+            />
+          </label>
+          <label className="label" htmlFor="author">
+            Author
+            <input
+              className="input"
+              id="author"
+              name="author"
+              type="text"
+              required
+            />
+          </label>
+          <div className="input-group">
+            <label className="label" htmlFor="pages">
+              Pages
+              <input
+                className="input"
+                id="pages"
+                name="pages"
+                type="number"
+                min="0"
+                required
+              />
+            </label>
+            <label className="label" htmlFor="is-read">
+              Read ?
+              <input
+                className="input"
+                id="is-read"
+                name="read"
+                type="checkbox"
+              />
+            </label>
+          </div>
+          <input className="submit" type="submit" value="Submit" />
+        </form>
+      </div>
+    </div>
+  );
+}
+
 class Library extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       books: sampleData,
+      showForm: false,
     };
-    this.handleDelete = this.handleDelete.bind(this);
     this.handleReadChange = this.handleReadChange.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleAddButtonClick = this.handleAddButtonClick.bind(this);
   }
 
   handleReadChange(id) {
@@ -109,8 +183,32 @@ class Library extends React.Component {
     });
   }
 
-  render() {
+  handleSubmit(event) {
+    event.preventDefault();
     const { books } = this.state;
+    const newBook = {
+      id: books[books.length - 1].id + 1,
+      title: event.target.title.value,
+      author: event.target.author.value,
+      pages: event.target.pages.value,
+      read: event.target['is-read'].checked,
+    };
+    const newBooks = books.concat(newBook);
+    this.setState({
+      showForm: false,
+      books: newBooks,
+    });
+    event.target.reset();
+  }
+
+  handleAddButtonClick() {
+    this.setState({
+      showForm: true,
+    });
+  }
+
+  render() {
+    const { showForm, books } = this.state;
     const renderedBooks = books.map((e) => {
       return (
         <Book
@@ -125,7 +223,13 @@ class Library extends React.Component {
         />
       );
     });
-    return <div className="library">{renderedBooks}</div>;
+    return (
+      <div className="library">
+        {renderedBooks}
+        <NewBook onClick={this.handleAddButtonClick} />
+        <Form showForm={showForm} handleSubmit={this.handleSubmit} />
+      </div>
+    );
   }
 }
 
